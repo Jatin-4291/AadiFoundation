7d72d2aa35805ae36ad493142e699bd3796f768b
Jatin-4291 <jatin.dua2003@gmail.com>
2024-07-10 23:31:16 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import React, { useState } from "react";
-import FacebookLogin from "react-facebook-login";
+import FacebookLogin from "@greatsumini/react-facebook-login";
 import axios from "axios";
 
 const App = () => {
